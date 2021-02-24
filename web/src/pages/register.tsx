0a8d4c3ee185f8b2b90fc0b9d466dfb7a42acbaa
taiga <taiga.similarities@gmail.com>
2021-02-25 06:05:43 +0900

    6_urql-graph-codegen-register-error

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -10,16 +10,65 @@ import {
 } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+
+import { useRouter } from "next/router";
+
 
 interface registerProps {}
 
+
+/* conncet to database throgh graphql but not need to any more 
+
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!){
+  register(options:{ username: $username , password:$password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id 
+      username
+    }
+  }
+}
+`;
+*/
+
 const Register: React.FC<registerProps> = ({}) => {
+
+  /* before add graphql mutations
+  const [,register] = useMutation(REGISTER_MUT);
+  */
+
+  const router = useRouter();
+
+  const [, register] = useRegisterMutation();
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          // console.log(values);
+        onSubmit={ async (values, {setErrors}) => {
+          const response = await register(values);
+          
+          //confirm error 
+          if(response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            
+            // console.log(response.data?.register.user);
+            router.push("/");
+          }
+
+          /* 
+            //confirm values whether actually working or not 
+            console.log(values);
+            register(values)
+          */
         }}
       >
         {({ isSubmitting }) => (
