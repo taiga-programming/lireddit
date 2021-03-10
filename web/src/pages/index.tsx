be0d048c76206d47969835f9ee3dce504268eaf2
taiga <taiga.similarities@gmail.com>
2021-03-10 18:36:58 +0900

    add web 18_style-posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,31 +4,49 @@ import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from "../generated/graphql";
 import React from 'react';
 import { Layout } from '../components/Layout';
-import { Link } from '@chakra-ui/core';
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
 import NextLink from "next/link";
 
-
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
 
+  if(!fetching && !data ) {
+    return <div>you got no posts for some reason</div>
+  }
+
   return (
    <Layout>
-     <NextLink href='/create-post'>
-      <Link>
-      create post
-      </Link>
-     </NextLink>
+    <Flex align='center'>
+      <NextLink href='/create-post'>
+        <Link ml='auto'>create post</Link>
+      </NextLink>
+    </Flex>
      <br/>
-     {!data ? (
+     {!data && !fetching ? (
        <div>loading....</div>
      ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+       //data! means (Not need to ask.value is already declared)
+      <Stack spacing={8}>
+        {data!.posts.map((p) => (
+          <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+             <Heading fontSize="xl">{p.title}</Heading>
+             textSnippet
+
+              <Text mt={4}>{p.textSnippet}</Text>
+             {/* <Text mt={4}>{p.text.slice(0, 50)}</Text> */}
+          </Box>
+      ))}
+       </Stack>
      )}
-   </Layout>
+     { data ? (<Flex>
+        <Button m= "auto" isLoading={fetching} my={4}> load more</Button>
+     </Flex>
+     ): null }
+     </Layout>
    
   );
 };
