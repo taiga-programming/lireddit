74b171317c53c9201757be9a1d843d096f313326
taiga <taiga.similarities@gmail.com>
2021-03-25 23:00:10 +0900

    add server 29_ssr-cookie

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -113,10 +113,18 @@ export class PostResolver {
     const reaLimitPlusOne = realLimit + 1;
 
     const replacements: any[] = [reaLimitPlusOne];
+    
 
+    if(req.session.userId) {
+      replacements.push(req.session.userId);
+    }
+
+    let cursorIdx = 3;
 
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
+      
     }
 
     const posts = await getConnection().query(
@@ -137,7 +145,7 @@ export class PostResolver {
     }
     from post p
     inner join public.user u on u.id = p."creatorId"
-    ${cursor ? `where p."createdAt" < $3` : ""}
+    ${cursor ? `where p."createdAt" < $${cursorIdx}` : ""}
     order by p."createdAt" DESC
     limit $1
     `,
